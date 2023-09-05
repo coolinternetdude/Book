e471823067379805dec8cca417f4f5f055ea672d
Younes <younesesebbar089@gmail.com>
2023-09-05 12:25:50 +0100

    intial commit

---
+++ server/seeds/author.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        firstname: "Victor",
+        lastname: "Hugo",
+        age: 69,
+        Books: ["This is my life", "Oppie"]
+    },
+    {
+        firstname: "JK",
+        lastname: "Rowling",
+        age: 69,
+        Books: ["This is my life", "Oppie"]
+    },
+    {
+        firstname: "Glenn",
+        lastname: "wilson",
+        age: 69,
+        Books: ["This is my life", "Oppie"]
+    },
+    {
+        firstname: "Charles",
+        lastname: "dawkins",
+        age: 69,
+        Books: ["This is my life", "Oppie"]
+    }
+]
---
+++ server/seeds/users.js
@@ -0,0 +1,25 @@
+module.exports = [{
+    username: "mkira",
+    password: "123",
+    email: "mkira@gmail.com"
+},
+{
+    username: "younes",
+    password: "123",
+    email: "younes@gmail.com"
+},
+{
+    username: "hamza",
+    password: "123",
+    email: "hamza@gmail.com"
+},
+{
+    username: "rachid",
+    password: "123",
+    email: "rachid@gmail.com"
+},
+{
+    username: "achraf",
+    password: "123",
+    email: "achraf@gmail.com"
+}]
