f7f548c197d5b268fe47b7df2d4e78b87767142c
ayoubmkira <ayoub.mkira@gmail.com>
2023-09-19 16:26:19 +0100

    Small change on book routes

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ app.set("view engine", "ejs");
 // Book Routes:
 app.route('/api/books').get(getAllBooks);
 app.route('/api/books/new').get(renderNewForm).post(createBook);
-app.route('/api/books/:id').get(showBook);
+app.route('/api/books/:id').get(showBook).put(updateBook).delete(deleteBook);
 app.route('/api/books/:id/edit').get(renderEditForm);
-app.route('/api/books/:id').put(updateBook).delete(deleteBook);
 
 
 app.listen(port, () => {
